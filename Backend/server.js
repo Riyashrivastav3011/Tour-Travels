b26ba115a83f8536eb5a6c7731ee50c4584fb6b8
Riya Shrivastav <engriyashrivastav3011@gmail.com>
2026-08-22 11:29:07 +0530

    urls

--- Backend/server.js
+++ Backend/server.js
@@ -27,7 +27,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
 
 app.use('/api', authRoutes);
 app.use('/admin', adminRoutes);
